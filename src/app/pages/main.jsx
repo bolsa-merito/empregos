50fb2bfeaeb2b673daeee110bb6f61e0addf39c4
hugobatista27 <hugobatista2710@gmail.com>
2024-11-25 11:19:38 -0300

    feat: importa e aplica o componente Empresa

--- src/app/pages/main.jsx
+++ src/app/pages/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import './global.css'
 import Index from './Index.jsx'
 import { Estudante } from './estudante/Estudante.jsx'
+import { Empresa } from './empresa/Empresa.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -12,7 +13,7 @@ createRoot(document.getElementById('root')).render(
 			<Routes>
 				<Route path='/' element={<Index/>}/>
 				<Route path='estudante' element={<Estudante/>}/>
-				<Route path='empresa' element={<Estudante/>}/>
+				<Route path='empresa' element={<Empresa/>}/>
 			</Routes>
 		</BrowserRouter>
     </StrictMode>
