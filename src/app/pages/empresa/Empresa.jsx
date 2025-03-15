83f01930646507d5cd379d33dc58794c3bf23d21
Hugo Batista <hugobatista2710@gmail.com>
2025-03-15 15:41:40 -0300

    feat: cria um exemplo de use de react

--- src/app/pages/empresa/Empresa.jsx
+++ src/app/pages/empresa/Empresa.jsx
@@ -1,10 +1,28 @@
 import { Link } from "react-router";
+import CardEstudante from "../../components/estudante/CardEstudantes";
+
+function CardEstudanteView() {
+    return (
+        <CardEstudante>
+            <CardEstudante.Banner/>
+            <div style={{padding: 16}}>
+                <CardEstudante.Nome/>
+                <CardEstudante.CompactDescription/>
+            </div>
+        </CardEstudante>
+    )
+}
 
 export const Empresa = () => {
     return (
         <div>
             <Link to={'/'}>&lt;</Link>
             <div>Empresa</div>
+            <div style={{display: "flex", flexDirection: "column", gap: 8}}>  
+                <CardEstudanteView/>
+                <CardEstudanteView/>
+                <CardEstudanteView/>
+            </div>
         </div>
     )
 }
