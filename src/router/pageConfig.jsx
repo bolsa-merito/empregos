c7208c9dde3b74a11caa146b39b53969ed72750c
Hugo Batista <hugobatista2710@gmail.com>
2025-04-18 20:19:08 -0300

    feat: configura as rotas da seção do estudante

--- src/router/pageConfig.jsx
+++ src/router/pageConfig.jsx
@@ -1,17 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import HomePage from "@/pages/home/HomePage";
+
+import StudentLayout from "@/components/layouts/StudentLayout"
 import StudentHome from "@/pages/student/StudentHome";
+import StudentProfile from "@/pages/student/StudentProfile";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <HomePage/>,
-        // children: [
-        //     {
-        //         path: 'estudante',
-        //         element: <div>Estudante</div>
-        //     }
-        // ]
+        element: <HomePage/>
     },
 
     // TODO proteger as rotas /estudante /empresa
@@ -26,7 +23,17 @@ const router = createBrowserRouter([
     // },
     {
         path: '/estudante',
-        element: <StudentHome/>
+        element: <StudentLayout/>,
+        children: [
+            {
+                path: 'home',
+                element: <StudentHome/>
+            },
+            {
+                path: 'perfil',
+                element: <StudentProfile/>
+            }
+        ]
     },
     {
         path: '/empresa',
