96fa6e6f10248a0fc3802ecdee0011e56b104c57
Hugo Batista <hugobatista2710@gmail.com>
2025-05-16 20:08:13 -0300

    style: ajusta o estilo do layout do Company

--- src/components/layouts/CompanyLayout.jsx
+++ src/components/layouts/CompanyLayout.jsx
@@ -1,10 +1,15 @@
 import NavBar from "@/components/navBar/NavBar"
 import { Outlet } from "react-router" // tem comportamento semelhante ao children
+import { getPublicStudents } from "../../features/company/services/fetchStudents.js";
 
 function CompanyLayout() {
+    getPublicStudents().then(data => {
+        console.log('Students:', data);
+    });
+
     return (
         <div className="w-full flex flex-col items-center">
-            <div className="w-full max-w-xs">
+            <div className="w-full md:max-w-screen max-w-xs">
                 <NavBar>
                     <NavBar.LinkHome redirectTo="/empresa/home">Home</NavBar.LinkHome>
                     <NavBar.LinkPerfil redirectTo="/empresa/perfil">Perfil</NavBar.LinkPerfil>
