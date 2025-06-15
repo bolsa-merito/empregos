c2daa15dd07475c6f878dc7819f24ca568ac6c35
Hugo Batista <hugobatista2710@gmail.com>
2025-06-15 15:42:04 -0300

    style: ajusta o padding

--- src/pages/company/FIlterStudents.jsx
+++ src/pages/company/FIlterStudents.jsx
@@ -1,13 +1,13 @@
 import AreaList from "@/features/company/components/studentFilter/AreaList"
 import CourseFilter from "@/features/company/components/studentFilter/CourseFilter"
 import { Link } from "react-router"
-import { StudentFilterProvider, useStudentFilter } from "@/features/company/hooks/StudentFilter"
+import { StudentFilterProvider } from "@/features/company/hooks/StudentFilter"
 import { ChevronLeft } from "lucide-react"
 import StudentCatalog from "@/features/company/components/studentCatalog/StudentCatalog"
 
 function FilterStudents() {
     return (
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 p-6">
             <Link to={"/empresa/home"}><ChevronLeft /></Link>
             <StudentFilterProvider>
                 <AreaList/>
